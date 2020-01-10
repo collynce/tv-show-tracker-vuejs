82aeeb1f206385856b3719219974d0dfa59d9638
Collins Kiprotich <collinsrono02@gmail.com>
2020-01-10 10:44:11 +0300

    Configured Firebase

---
+++ src/firebase/credentials.js
@@ -0,0 +1,12 @@
+export default {
+    firebase: {
+        apiKey: "AIzaSyD13BhwxdqJndn7KmiwikGoOSKSbaHYVDg",
+        authDomain: "tv-show-tracker-329b9.firebaseapp.com",
+        databaseURL: "https://tv-show-tracker-329b9.firebaseio.com",
+        projectId: "tv-show-tracker-329b9",
+        storageBucket: "tv-show-tracker-329b9.appspot.com",
+        messagingSenderId: "558667231422",
+        appId: "1:558667231422:web:7d6f0de94a8b4c83d62eef",
+        measurementId: "G-YSPVR0G5GH"
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import Vue from 'vue'
 import vuetify from './plugins/vuetify';
+import firebase from 'firebase';
+import store from './store/index'
+import router from './router/index'
+import './firebase/firebase'
 
-require('./js/components/components');
+require
 
-new Vue({
-    el: '#app',
-    vuetify
+('./js/components/components');
+
+
+let app;
+firebase.auth().onAuthStateChanged(user => {
+    if (!app) {
+        new Vue({
+            el: '#app',
+            store,
+            router,
+            vuetify
+        })
+    }
 });
